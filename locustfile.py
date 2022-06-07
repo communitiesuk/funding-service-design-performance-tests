d47d361e6e73997c4fe5d3bd804089d8d1aa56ed
Rayhan Chowdhury <rayhan.chowdhury@version1.com>
2022-06-07 14:17:34 +0100

    Removed further references to round store including removing test file

--- locustfile.py
+++ locustfile.py
@@ -1,3 +1,2 @@
 from my_locustfiles.application_store import ApplicationStore  # noqa
 from my_locustfiles.fund_store import FundStore  # noqa
-from my_locustfiles.round_store import RoundStore  # noqa
