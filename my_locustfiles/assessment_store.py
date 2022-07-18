61478270aa631d0a2c45b2910b1b75e859fe96e5
Rayhan Chowdhury <rayhan.chowdhury@version1.com>
2022-07-18 10:26:17 +0100

    Performance test for listing all asessments

--- my_locustfiles/assessment_store.py
+++ my_locustfiles/assessment_store.py
@@ -4,3 +4,17 @@ from common.common_methods import check_expected_status
 from common.config import ASSESSMENT_STORE
 from locust import HttpUser
 from locust import task
+
+class AssessmentStore(HttpUser):
+    host = ASSESSMENT_STORE
+
+
+    @task
+    def list_all_asessments(self):
+        """
+        Performance test for GET /assessments that expects a 200
+        """
+        with self.client.get("/assessments", catch_response=True) as response:
+            check_expected_status(response, 200)
+
+    
