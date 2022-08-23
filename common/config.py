f533dcbadf5daccd2694c705b2eb721cfac2a2f8
Rayhan Chowdhury <rayhan.chowdhury@version1.com>
2022-08-23 16:59:36 +0100

    change config to test

--- common/config.py
+++ common/config.py
@@ -2,7 +2,7 @@
 Application Config
 """
 FUND_STORE = (
-    "https://funding-service-design-fund-store-dev.london.cloudapps.digital"
+    "https://funding-service-design-fund-store-test.london.cloudapps.digital"
 )
-APPLICATION_STORE = "https://funding-service-design-application-store-dev.london.cloudapps.digital"  # noqa
+APPLICATION_STORE = "https://funding-service-design-application-store-test.london.cloudapps.digital"  # noqa
 AUTHENTICATOR = "https://funding-service-design-authenticator-test.london.cloudapps.digital" # noqa
