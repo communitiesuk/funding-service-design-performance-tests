0af7469c182f791fa4b1220eeae200ccf4f53fea
Rayhan Chowdhury <rayhan.chowdhury@version1.com>
2022-08-26 15:35:43 +0100

    post performance test for organisation information

--- my_locustfiles/forms.py
+++ my_locustfiles/forms.py
@@ -8,6 +8,10 @@ from locust import task
 class Forms(HttpUser):
 
     host = FORMS
+    new_application_json_file = open(
+        "./data/forms/organisation-information.json", "r"
+    )
+    new_application_json = json.loads(new_application_json_file.read())
 
 
 # Click new application and tasklist
@@ -20,3 +24,13 @@ class Forms(HttpUser):
             "/organisation-information/about-your-organisation", headers={"Authorization":"Basic ZnNkOmZzZA=="}, catch_response=True
         ) as response:
             check_expected_status(response, 200)
+
+
+    @task
+    def post_organisation_information(self):
+        """
+        Performance test for POST new application and organisation_information that expects a 201
+        """
+        with self.client.put("/organisation-information/about-your-organisation", json={self.new_application_json}, catch_response=True
+        ) as response:
+            check_expected_status(response, 201)
