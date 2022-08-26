720f1699381a400fb4d1d0e985e8baa56c25e0e1
Rayhan Chowdhury <rayhan.chowdhury@version1.com>
2022-08-26 16:28:01 +0100

    performance test for one form per section

--- my_locustfiles/forms.py
+++ my_locustfiles/forms.py
@@ -8,11 +8,6 @@ from locust import task
 class Forms(HttpUser):
 
     host = FORMS
-    new_application_json_file = open(
-        "./data/forms/organisation-information.json", "r"
-    )
-    new_application_json = json.loads(new_application_json_file.read())
-
 
 # Click new application and tasklist
     @task
@@ -25,12 +20,76 @@ class Forms(HttpUser):
         ) as response:
             check_expected_status(response, 200)
 
-
     @task
-    def post_organisation_information(self):
+    def get_project_information(self):
         """
-        Performance test for POST new application and organisation_information that expects a 201
+        Performance test for GET project information that expects a 200
         """
-        with self.client.put("/organisation-information/about-your-organisation", json={self.new_application_json}, catch_response=True
+        with self.client.get(
+            "/project-information/about-your-project", headers={"Authorization":"Basic ZnNkOmZzZA=="}, catch_response=True
         ) as response:
-            check_expected_status(response, 201)
+            check_expected_status(response, 200)
+    
+    @task
+    def get_community_use(self):
+        """
+        Performance test for GET community use that expects a 200
+        """
+        with self.client.get(
+            "/community-use/strategic-case-aMRtTs", headers={"Authorization":"Basic ZnNkOmZzZA=="}, catch_response=True
+        ) as response:
+            check_expected_status(response, 200)
+    
+    @task
+    def get_funding_required(self):
+        """
+        Performance test for GET funding required that expects a 200
+        """
+        with self.client.get(
+            "/funding-required/management-case", headers={"Authorization":"Basic ZnNkOmZzZA=="}, catch_response=True
+        ) as response:
+            check_expected_status(response, 200)
+    
+    @task
+    def get_community_benefits(self):
+        """
+        Performance test for GET community benefits that expects a 200
+        """
+        with self.client.get(
+            "/community-benefits/potential-to-deliver-community-benefits-rofqAG", headers={"Authorization":"Basic ZnNkOmZzZA=="}, catch_response=True
+        ) as response:
+            check_expected_status(response, 200)
+    
+    @task
+    def get_value_to_the_community(self):
+        """
+        Performance test for GET value to the communityu that expects a 200
+        """
+        with self.client.get(
+            "/value-to-the-community/added-value-to-the-community-XXYtSd", headers={"Authorization":"Basic ZnNkOmZzZA=="}, catch_response=True
+        ) as response:
+            check_expected_status(response, 200)
+    
+    @task
+    def get_project_qualification(self):
+        """
+        Performance test for GET project qualification that expects a 200
+        """
+        with self.client.get(
+            "/project-qualification/subsidy-control-and-state-aid-XQBpkr", headers={"Authorization":"Basic ZnNkOmZzZA=="}, catch_response=True
+        ) as response:
+            check_expected_status(response, 200)
+    
+    @task
+    def get_declaration(self):
+        """
+        Performance test for GET declarations that expects a 200
+        """
+        with self.client.get(
+            "/declarations/declarations-YcIHbL", headers={"Authorization":"Basic ZnNkOmZzZA=="}, catch_response=True
+        ) as response:
+            check_expected_status(response, 200)
+    
+    
+    
+  
