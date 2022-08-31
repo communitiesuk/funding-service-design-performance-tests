fa017e377bb3016940f60ad838d0e51048a42a43
Rayhan Chowdhury <rayhan.chowdhury@version1.com>
2022-08-31 15:31:57 +0100

    Added username and password

--- common/config.py
+++ common/config.py
@@ -6,7 +6,6 @@ FUND_STORE = (
 )
 APPLICATION_STORE = "https://funding-service-design-application-store-test.london.cloudapps.digital"  # noqa
 AUTHENTICATOR = "https://funding-service-design-authenticator-test.london.cloudapps.digital" # noqa
-FRONTEND = "https://frontend.test.gids.dev" # noqa
-FORMS = "https://forms.test.gids.dev" # noqa
+FRONTEND = "https://fsd:fsd@frontend.test.gids.dev" # noqa
+FORMS = "https://fsd:fsd@forms.test.gids.dev" # noqa
 ACCOUNT = "https://funding-service-design-account-store-test.london.cloudapps.digital" # noqa
-AUTHORIZATION = "Authorization: Basic ZnNkOmZzZA=="
