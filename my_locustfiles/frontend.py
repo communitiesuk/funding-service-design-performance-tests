a021ca9a3f41e54439153002090ceb8bff9b6825
Rayhan Chowdhury <rayhan.chowdhury@version1.com>
2022-10-06 16:45:22 +0100

    new cookie

--- my_locustfiles/frontend.py
+++ my_locustfiles/frontend.py
@@ -41,7 +41,7 @@ class FrontEnd(HttpUser):
         Performance test for GET new application that expects a 200
         """
         with self.client.get(
-            "/tasklist/dfa16c14-f6d0-4f8b-83a4-bb0f4badb03b",headers={"cookie": "session_cookie=eyJjc3JmX3Rva2VuIjoiMzQ0MWM1OWYyODlkMWJlMjUyZWVkYmQ4NWJhNTU5YWFhYzExOGQyYyJ9.Yw3hBg.W2E7Kq5gg3eKOgdaSuRjBUBiFIY; fsd_user_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhY2NvdW50SWQiOiI5YTFlNGEwZi0zZjA2LTRkNzQtOWRmOC01NjI0ZTU0N2NmN2QiLCJpYXQiOjE2NjI5NzI1NTcsImV4cCI6MTY2MzA1ODk1N30.JGfiWv7LNN0ZVzZGUj8aSWROtM6IpWVI_xDI2uDRntvfr3A_oxOekLg6Nxa5RvUf1YSsF4fCBXqLl2XnF41OMAzcTtny0OHXHKRxKqXbujaIZMMPiWJiwUuWCOVhYrac9uXt5IR75ZmtVBN65H7fHFxq03sVRAQuMVHXGjAxr1g"}, catch_response=True
+            "/tasklist/dfa16c14-f6d0-4f8b-83a4-bb0f4badb03b",headers={"cookie": "'session_cookie=eyJjc3JmX3Rva2VuIjoiMzQ0MWM1OWYyODlkMWJlMjUyZWVkYmQ4NWJhNTU5YWFhYzExOGQyYyJ9.Yw3hBg.W2E7Kq5gg3eKOgdaSuRjBUBiFIY; fsd_user_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhY2NvdW50SWQiOiI5YTFlNGEwZi0zZjA2LTRkNzQtOWRmOC01NjI0ZTU0N2NmN2QiLCJpYXQiOjE2NjI5NzI1NTcsImV4cCI6MTY2MzA1ODk1N30.JGfiWv7LNN0ZVzZGUj8aSWROtM6IpWVI_xDI2uDRntvfr3A_oxOekLg6Nxa5RvUf1YSsF4fCBXqLl2XnF41OMAzcTtny0OHXHKRxKqXbujaIZMMPiWJiwUuWCOVhYrac9uXt5IR75ZmtVBN65H7fHFxq03sVRAQuMVHXGjAxr1g'"}, catch_response=True
         ) as response:
             check_expected_status(response, 200)
     
