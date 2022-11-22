aaa023451046f0dab61d42407de4e84d4ebe3a6c
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2022-11-22 15:48:11 +0000

    reading target urls from env

--- common/config.py
+++ common/config.py
@@ -1,7 +1,14 @@
 """
 Application Config
 """
-FUND_STORE = (
-    "https://funding-service-design-fund-store-dev.london.cloudapps.digital"
+from os import getenv
+
+
+FUND_STORE = getenv(
+    "TARGET_URL_FUND_STORE",
+    "https://funding-service-design-fund-store-dev.london.cloudapps.digital",
 )
-APPLICATION_STORE = "https://funding-service-design-application-store-dev.london.cloudapps.digital"  # noqa
+APPLICATION_STORE = getenv(
+    "TARGET_URL_APPLICATION_STORE",
+    "https://funding-service-design-application-store-dev.london.cloudapps.digital",  # noqa
+)
