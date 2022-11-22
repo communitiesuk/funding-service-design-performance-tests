9ba07b1121ec8e894f331a138665dafd01f2345a
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2022-11-22 16:04:57 +0000

    adding printouts

--- common/config.py
+++ common/config.py
@@ -12,3 +12,7 @@ APPLICATION_STORE = getenv(
     "TARGET_URL_APPLICATION_STORE",
     "https://funding-service-design-application-store-dev.london.cloudapps.digital",  # noqa
 )
+
+print("======= URLs ==========")
+print(f"FUND_STORE: {FUND_STORE}")
+print(f"APPLICATION_STORE: {APPLICATION_STORE}")
