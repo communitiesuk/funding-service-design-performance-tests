bec78131b5f42b4f1b66f17e72a9e5d12696a95f
Rayhan Chowdhury <rayhan.chowdhury@version1.com>
2023-02-14 11:54:24 +0000

    add config for assessment store

--- common/config.py
+++ common/config.py
@@ -13,6 +13,12 @@ APPLICATION_STORE = getenv(
     "https://funding-service-design-application-store-dev.london.cloudapps.digital",  # noqa
 )
 
+ASSESSMENT_STORE = getenv (
+    "TARGET_URL_ASESSMENT_STORE",
+    "https://funding-service-design-assessment-store-dev.london.cloudapps.digital/",
+)
+
 print("======= URLs ==========")
 print(f"FUND_STORE: {FUND_STORE}")
 print(f"APPLICATION_STORE: {APPLICATION_STORE}")
+print(f"ASSESSMENT_STORE:  {ASSESSMENT_STORE}")
