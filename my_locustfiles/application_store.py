371e674a9fad6b24aac03365b7a7bfbf423fc31a
Rayhan Chowdhury <rayhan.chowdhury@version1.com>
2023-11-02 17:37:53 +0000

    Test for reporting endpoint

--- my_locustfiles/application_store.py
+++ my_locustfiles/application_store.py
@@ -40,15 +40,15 @@ class ApplicationStore(HttpUser):
         ) as response:
              check_expected_status(response, 200)
     
-    # @task
-    # def get_report_on_started_and_submitted_applications_for_a_fund(self):
-    #     """
-    #     Performance test for GET /applications?fund_id={fund_id} that expects a 200
-    #     """
-    #     with self.client.get(
-    #         "/applications?fund_id=''",
-    #         headers={"User-Agent": USER_AGENT},
-    #         catch_response=True,
-    #     ) as response:
-    #          check_expected_status(response, 200)
+    @task
+    def get_report_on_started_and_submitted_applications_for_a_fund(self):
+        """
+        Performance test for GET /applications/reporting/applications_statuses_data that expects a 200
+        """
+        with self.client.get(
+            "/applications/reporting/applications_statuses_data",
+            headers={"User-Agent": USER_AGENT},
+            catch_response=True,
+        ) as response:
+             check_expected_status(response, 200)
 
